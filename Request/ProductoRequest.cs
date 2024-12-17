989c0ed25e27f4ae57c0b1907c80a6355b58b5f0
Santos Pairazaman <spairazaman@hotmail.com>
2024-12-16 19:49:13 -0500

    Creación de Request y Response

---
+++ Request/ProductoRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_05.Request
+{
+    public class ProductoRequest
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        public int CategoriaID { get; set; }
+    }
+}
---
+++ Response/ProductoResponse.cs
@@ -0,0 +1,13 @@
+﻿using Examen_05.Models;
+
+namespace Examen_05.Response
+{
+    public class ProductoResponse
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        public string Categoria { get; set; }
+    }
+}
